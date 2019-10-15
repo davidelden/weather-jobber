9e58e931d6550fc29633ab4872447d2e7dcf80cd
davidelden <deelden+github@gmail.com>
2019-10-15 13:38:09 -0500

    add streams: [actions, events], shell scripts

---
+++ src/streams/events/eventMessages.js
@@ -0,0 +1,11 @@
+const eventMessages = {
+  eastern: ['message', '7AM-USEastern'],
+  central: ['message', '7AM-USCentral'],
+  mountain: ['message', '7AM-USMountain'],
+  arizona: ['message', '7AM-Arizona'],
+  pacific: ['message', '7AM-USPacific'],
+  alaska: ['message', '7AM-USAlaska'],
+  hawaii: ['message', '7AM-USHawaii']
+}
+
+module.exports = eventMessages;
